be0130df714bbca78921e931f744a31299c07111
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-12-03 19:05:43 +0000

    cat evo ! - I know you shouldn't do this, but I am in too deep of this joke atm

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,17 @@
-'use_strict';
-
 const request = require('request');
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (err, res, body) => {
-  if (err) {
-    throw new Error('Web access failed: ', err, res);
-  }
-  let data = JSON.parse(body);
-  if (!data[0]) {
-    throw new Error(`Breed: ${args[0]} Not Found`);
-  }
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function(breed, cb) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, res, body) => {
+    if (err) {
+      cb(err);
+    }
+    let data = JSON.parse(body);
+    if (!data[0] || data[0].name !== breed) {
+      cb("Breed: " + breed + " is not found");
+    } else {
+      cb(null ,data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
